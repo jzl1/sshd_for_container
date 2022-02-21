651e489e722fa9f06f9fbcd4fea76b30c2678adb
jzl1 <34795003+jzl1@users.noreply.github.com>
2022-02-21 18:39:01 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo root:$pwd|chpasswd
 nohup /usr/sbin/sshd -D -p 22 &
 nohup /nezha-agent -s $tz_ip -p $tz_secret &
 echo -e "[common]\nserver_addr = ${server_addr}\nserver_port = ${server_port}\ntoken = ${token}\n\n[$RANDOM]\ntype = tcp\nlocal_ip = 127.0.0.1\nlocal_port = 22\nremote_port = ${remote_port}" >>/etc/frp/frpc.ini
